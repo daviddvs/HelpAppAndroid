51e75c2ec4b69b4a3ed36b5da791c60863d805f5
David Franco <dfranco009@ikasle.ehu.eus>
2018-01-07 11:58:02 +0100

    Finished basic layout of main activity

---
+++ app/src/main/java/eus/ehu/helpappandroid/helpappandroid/BilbaoActivity.java
@@ -0,0 +1,13 @@
+package eus.ehu.helpappandroid.helpappandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BilbaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bilbao);
+    }
+}
