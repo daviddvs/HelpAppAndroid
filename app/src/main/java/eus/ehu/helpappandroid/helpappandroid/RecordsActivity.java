55d6b3de5498b5916af6d1686f402032c1ddf8ee
David Franco <dfranco009@ikasle.ehu.eus>
2018-01-26 17:18:07 +0100

    Record function moving and renaming files

--- app/src/main/java/eus/ehu/helpappandroid/helpappandroid/RecordsActivity.java
+++ app/src/main/java/eus/ehu/helpappandroid/helpappandroid/RecordsActivity.java
@@ -11,6 +11,7 @@ import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
@@ -20,9 +21,13 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import modelo.ProgressTask;
+
 public class RecordsActivity extends AppCompatActivity {
 
     public static final int AUDIO_REQUEST_CODE = 2;
+    private String audioName;
+    private String audioPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +36,40 @@ public class RecordsActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
         if(resultCode != Activity.RESULT_OK)
             return;
         if(requestCode == AUDIO_REQUEST_CODE) {
             // data.getData(): Esto devuelve uri en fromato content://
             // Lo que hay que hacer es coger el archivo, leerlo con un input stream y escribirlo con output stream en otro dir que yo elija
-            String filePath = "eus.ehu.helpappandroid/records";//modificar esto
-            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+            //String filePath = "eus.ehu.helpappandroid/records";//modificar esto
+            final File dirMusic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+            audioPath = dirMusic+"/HelpAppAudio";
             //File dir = Environment.getDataDirectory();
-            moveFile(data.getData(),dir.getPath(),"prueba1"+".amr");
+            /*
+            String filepath;
+            Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
+            if (cursor == null) { // Source is Dropbox or other similar local file path
+                filepath = data.getData().getPath();
+            } else {
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                filepath = cursor.getString(idx);
+                cursor.close();
+            }
+            */
+
+            new ProgressTask<String>(this){
+                @Override
+                protected String work() throws Exception{
+                    return moveFile(data.getData(),audioPath,audioName+".mp3");
+                }
+                @Override
+                protected void onFinish(String result){
+                    Toast.makeText(context, "Moved file: "+result, Toast.LENGTH_SHORT).show();
+                }
+            }.execute();
+
         }
     }
 
@@ -50,8 +79,11 @@ public class RecordsActivity extends AppCompatActivity {
         }
         else {
             Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
-            if (intent.resolveActivity(getPackageManager()) != null)
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                EditText audioNameText = (EditText)findViewById(R.id.audio_name);
+                audioName =  audioNameText.getText().toString();
                 startActivityForResult(intent, AUDIO_REQUEST_CODE);
+            }
             else
                 Toast.makeText(this,R.string.no_app, Toast.LENGTH_SHORT).show();
         }
@@ -59,7 +91,17 @@ public class RecordsActivity extends AppCompatActivity {
     }
 
     public void openRecords(View view) {
+        /*
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        startActivity(intent);
+        */
 
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Uri uri = Uri.parse(audioPath);
+        intent.setDataAndType(uri, "*/*");
+        startActivity(Intent.createChooser(intent, "Open folder"));
     }
 
     public void back(View view) {
@@ -67,15 +109,15 @@ public class RecordsActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void moveFile(Uri inputUri, String outputPath, String outputName) {
+    private String moveFile(Uri inputUri, String outputPath, String outputName) {
 
         File dir = new File(outputPath);
         if(!dir.exists())
             dir.mkdirs();
 
         try {
-            //InputStream is = getContentResolver().openInputStream(inputUri);
-            InputStream is = new FileInputStream(inputUri.getPath());
+            InputStream is = getContentResolver().openInputStream(inputUri);
+            //InputStream is = new FileInputStream(inputUri);
             OutputStream os = new FileOutputStream(outputPath+"/"+outputName);
 
             byte[] buffer = new byte[1024];
@@ -89,10 +131,13 @@ public class RecordsActivity extends AppCompatActivity {
             os.close();
             os=null;
 
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return outputPath+"/"+outputName;
     }
 }
